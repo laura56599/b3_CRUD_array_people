547bf6c6b777147d3bf4d6d4d7d4f42a029ae4c0
laura.velez <lauravelez1991@hotmail.com>
2024-10-06 19:54:01 -0500

    Version final

--- src/components/People.jsx
+++ src/components/People.jsx
@@ -1,7 +1,85 @@
 import PropTypes from "prop-types";
 import { Person } from "./Person";
+import { useState } from "react";
 
-export const People = ( { people, setPeople } ) => {
+export const People = ({ people, setPeople }) => {
+
+  const [editingId, setEditingId] = useState(null);
+
+  const [isEditing, setIsEditing] = useState(false);
+
+  const [editedPerson, setEditedPerson] = useState(
+    {
+      name: '',
+      role: '',
+      img: ''
+    }
+  );
+
+  const [personToDelete, setPersonToDelete] = useState(null);
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setEditedPerson(prevState => ({
+      ...prevState,
+      [name]: value
+    }));
+  };
+
+  const handleCreate = (e) => {
+    e.preventDefault();
+
+    setPeople([...people, { id: people.length + 1, ...editedPerson }]);
+
+    setEditedPerson({ name: '', role: '', img: '' });
+  };
+
+  const handleEdit = (id) => {
+
+    setEditingId(id);
+    setIsEditing(true);
+    const personToEdit = people.find(person => person.id === id);
+
+    setEditedPerson({ ...personToEdit });
+  };
+
+  const handleSave = (e) => {
+
+    e.preventDefault();
+
+    const updatePeople = people.map(person => person.id === editingId ? editedPerson : person);
+
+    setPeople(updatePeople);
+
+    setIsEditing(false);
+
+    setEditingId(null);
+
+    setEditedPerson({
+      name: '',
+      role: '',
+      img: ''
+    });
+  }
+
+  // Métodos para eliminar una persona del array
+
+  const handleDelete = (id) => {
+    setPersonToDelete(id);
+  };
+
+  const confirmDelete = () => {
+
+    setPeople(people.filter(person => person.id !== personToDelete));
+
+    setPersonToDelete(null);
+  };
+
+  const cancelDelete = () => {
+    setPersonToDelete(null);
+  };
+
+
   return (
     <div>
       <h2 className='text-center my-4'>IT Team</h2>
@@ -16,11 +94,52 @@ export const People = ( { people, setPeople } ) => {
                     name={people.name}
                     img={people.img}
                     role={people.role}
+                    handleEdit={() => handleEdit(people.id)}
+                    handleDelete={() => handleDelete(people.id)}
                   />
                 </div>
               );
             })
           }
+        </div>
+      </div>
+      {/* Formulario */}
+      <div className='container'>
+        <h2 className='text-center mt-4' > {isEditing ? 'Actualizar Empleado' : 'Crear Nuevo Empleado'} </h2>
+        <form>
+          <div>
+            <label htmlFor="name">Nombres</label>
+            <input type="text" name="name" value={editedPerson.name} onChange={handleChange} required className="form-control" />
+          </div>
+          <div>
+            <label htmlFor="role">Rol</label>
+            <input type="text" name="role" value={editedPerson.role} onChange={handleChange} required className="form-control" />
+          </div>
+          <div>
+            <label htmlFor="img">Avatar</label>
+            <input type="text" name="img" value={editedPerson.img} onChange={handleChange} required className="form-control" />
+          </div>
+          <div className="mt-2 text-center">
+            <button type="submit" className="btn btn-primary" onClick={isEditing ? handleSave : handleCreate}> {isEditing ? 'Actualizar' : 'Crear'} </button>
+          </div>
+        </form>
+      </div>
+      {/* Modal de confirmación  para eliminar */}
+      <div id="deleteModal" className="modal fade" tabIndex="-1" >
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h4 className="modal-title">Confirmar Eliminación</h4>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" onClick={cancelDelete}></button>
+            </div>
+            <div className="modal-body">
+              <p>¿Estás seguro de eliminar a {people.find(person => person.id === personToDelete)?.name} ?</p>
+            </div>
+            <div className="modal-footer">
+              <button type="button" className="btn btn-secondary" data-bs-dismiss="modal" onClick={cancelDelete}>Cancelar</button>
+              <button type="button" className="btn btn-danger" data-bs-dismiss="modal" onClick={confirmDelete}>Eliminar</button>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/components/Person.jsx
+++ src/components/Person.jsx
@@ -1,17 +1,17 @@
 import PropTypes from "prop-types";
 
-export const Person = ({ id, name, role, img }) => {
+export const Person = ({ name, role, img, handleEdit, handleDelete }) => {
   return (
     <div className='col'>
-      <div className="card" style={{width: "18rem;"}}>
+      <div className="card mb-4" style={{width: "18rem"}}>
         <img src={img} className="card-img-top" alt={name} />
           <div className="card-body">
             <h3 className="card-title">{name}</h3>
             <p className="card-text">{role}</p>
           </div>
           <div className='container mb-4'>
-            <button className="btn btn-success mx-2">Editar</button>
-            <button className="btn btn-danger">Eliminar</button>
+            <button className="btn btn-primary mx-2" onClick={handleEdit}>Editar</button>
+            <button className="btn btn-danger" onClick={handleDelete} data-bs-toggle="modal" data-bs-target="#deleteModal" >Eliminar</button>
           </div>
       </div>
     </div>
@@ -19,8 +19,9 @@ export const Person = ({ id, name, role, img }) => {
 };
 
 Person.propTypes = {
-  id: PropTypes.number,
   name: PropTypes.string,
   role: PropTypes.string,
-  img: PropTypes.string
+  img: PropTypes.string,
+  handleEdit: PropTypes.func,
+  handleDelete: PropTypes.func
 }
